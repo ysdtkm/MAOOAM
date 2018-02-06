0ecbda128da0c74e7b12f274c4a3e094a972ace6
Takuma Yoshida <takuma@github.noreply>
2018-02-06 16:11:58 -0500

    MAOOAM/fortran exec.sh auto commit: experiment m071

--- fortran/exec.sh
+++ fortran/exec.sh
@@ -3,15 +3,14 @@
 set -e
 
 wdir_base="/lustre/tyoshida/shrt/exec"
-modeldir="/lustre/tyoshida/repos/works/MAOOAM/fortran"
+modeldir="/lustre/tyoshida/prgm/MAOOAM/fortran"
 
 # preparation
 word=m`/lustre/tyoshida/repos/python/pythonpath/oneliner/serial`
 wdir="${wdir_base}/${word}"
 
 cd ${modeldir}
-git add .
-git commit --allow-empty -m "MAOOAM/fortran exec.sh auto commit: experiment ${word}"
+git commit -a --allow-empty -m "MAOOAM/fortran exec.sh auto commit: experiment ${word}"
 
 echo "preparing files at ${wdir}"
 rm -rf ${wdir}
@@ -23,7 +22,7 @@ cp -f ${modeldir}/*.nml .
 
 echo "#!/bin/bash"                  > tmp.sh
 echo "#SBATCH -n 1"                >> tmp.sh
-echo "#SBATCH -t 9:30:00"          >> tmp.sh
+echo "#SBATCH -t 0:30:00"          >> tmp.sh
 echo "#SBATCH -J ${word}"          >> tmp.sh
 echo "set -e"                      >> tmp.sh
 echo "./maooam"                    >> tmp.sh
