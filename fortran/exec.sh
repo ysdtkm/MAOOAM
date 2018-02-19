7268a3daf0440ef5db0c36aa6c812ab9f856ed72
Takuma Yoshida <takuma@github.noreply>
2018-02-19 15:28:02 -0500

    MAOOAM/fortran exec.sh auto commit: experiment m101

--- fortran/exec.sh
+++ fortran/exec.sh
@@ -23,7 +23,7 @@ cp -f ${modeldir}/*.nml .
 
 echo "#!/bin/bash"                  > tmp.sh
 echo "#SBATCH -n 1"                >> tmp.sh
-echo "#SBATCH -t 0:30:00"          >> tmp.sh
+echo "#SBATCH -t 5:30:00"          >> tmp.sh
 echo "#SBATCH -J ${word}"          >> tmp.sh
 echo "set -e"                      >> tmp.sh
 # echo "./maooam"                    >> tmp.sh
