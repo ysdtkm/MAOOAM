c6970b27cc0dfcceff4606cde1957c5ca165454f
Takuma Yoshida <takuma@github.noreply>
2018-02-19 15:07:13 -0500

    mod

--- fortran/plot_tlm.py
+++ fortran/plot_tlm.py
@@ -6,7 +6,7 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 import subprocess
 
-N = 228
+N = 228   # atm 6x6 ocn 6x6
 DT = 1.0  # write interval in [timeunit]
 NT = 100  # number of write. filesize = 8 * (N ** 2 + N) * NT [bytes]
 ONEDAY = 8.64  # [timeunit/day] a46p51
