865ba3e7f14db896a537ee498b757c794fcd6983
Takuma Yoshida <takuma@github.noreply>
2018-02-19 13:49:22 -0500

    mod

--- fortran/plot_tlm.py
+++ fortran/plot_tlm.py
@@ -1,48 +1,47 @@
 #!/usr/bin/env python
 
-import os
 import numpy as np
 import matplotlib as mpl
-# mpl.use('Agg')
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 import subprocess
 
-NMODEL = 228
+N = 228
 DT = 1.0  # write interval in [timeunit]
-NT = 100
-
+NT = 100  # number of write. filesize = 8 * (N ** 2 + N) * NT [bytes]
 ONEDAY = 8.64  # [timeunit/day] a46p51
 
 def main():
     np.set_printoptions(formatter={'float': '{: 0.5f}'.format})
     mkdirs()
     fname = "evol_field_tlm.dat"
-    all_blv = np.empty((NT, NMODEL, NMODEL))
-    blv = np.random.normal(0.0, 1.0, (NMODEL, NMODEL))
+    all_blv = np.empty((NT, N, N))
+    blv = np.random.normal(0.0, 1.0, (N, N))
     blv, ble = orth_norm_vectors(blv)
-    all_ble = np.zeros((NT, NMODEL))
+    all_ble = np.zeros((NT, N))
     for i in range(0, NT):
         traj, tlm = read_file_part(fname, i)
         blv = np.dot(tlm[:, :], blv)
         blv, ble = orth_norm_vectors(blv)
         all_ble[i, :] = ble[:] / (DT / ONEDAY)
         all_blv[i, :, :] = blv[:, :]
-    plt.plot(np.mean(all_ble, axis=0))
-    print(np.mean(all_ble, axis=0))
+    mean_ble = np.mean(all_ble[NT//2:, :], axis=0)
+    plt.plot(mean_ble)
+    print(mean_ble)
     plt.savefig("img/tmp.pdf")
 
 def orth_norm_vectors(lv):
-  # lv     <- np.array[NMODEL,NMODEL] : Lyapunov vectors (column)
-  # return -> np.array[NMODEL,NMODEL] : orthonormalized LVs in descending order
-  # return -> np.array[NMODEL]      : ordered Lyapunov Exponents
+  # lv     <- np.array[N,N] : Lyapunov vectors (column)
+  # return -> np.array[N,N] : orthonormalized LVs in descending order
+  # return -> np.array[N]      : ordered Lyapunov Exponents
 
   q, r = np.linalg.qr(lv)
 
-  le = np.zeros(NMODEL)
+  le = np.zeros(N)
   eigvals = np.abs(np.diag(r))
 
   # for t-continuity, align
-  for i in range(NMODEL):
+  for i in range(N):
     inner_prod = np.dot(q[:,i].T, lv[:,i])
     if (inner_prod < 0):
       q[:,i] *= -1.0
@@ -56,10 +55,10 @@ def mkdirs():
     subprocess.run("mkdir -p img", check=True, shell=True)
 
 def read_file_part(fname, it):
-    dbyte = it * (NMODEL ** 2 + NMODEL) * 8
-    x = np.memmap(fname, offset=dbyte, dtype=np.float64, mode='r', shape=(NMODEL ** 2 + NMODEL))
-    traj = x[:NMODEL]
-    tlm = x[NMODEL:].reshape((NMODEL, NMODEL))
+    dbyte = it * (N ** 2 + N) * 8
+    x = np.memmap(fname, offset=dbyte, dtype=np.float64, mode='r', shape=(N ** 2 + N))
+    traj = x[:N]
+    tlm = x[N:].reshape((N, N))
     return traj, tlm
 
 if __name__ == "__main__":
