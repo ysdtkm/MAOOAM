dcd55444f825facf33f8f12601462e8d16b96351
Takuma Yoshida <takuma@github.noreply>
2018-02-19 16:49:00 -0500

    mod

--- fortran/aux/mod.py
+++ fortran/aux/mod.py
@@ -1,6 +1,6 @@
-with open("IC_atm66_ocn99.txt") as f:
+with open("IC_atm99_ocn66.txt") as f:
     a = f.read().split()
-with open("IC_atm66_ocn99mod.txt", "w") as f:
+with open("IC_atm99_ocn66mod.txt", "w") as f:
     for c in a:
         f.write(c)
         f.write("\n")
