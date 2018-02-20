470efbcda51c349d26c1924d65b26b0787d0ca64
Takuma Yoshida <takuma@github.noreply>
2018-02-20 10:40:48 -0500

    mod

--- fortran/plot_tlm.py
+++ fortran/plot_tlm.py
@@ -6,18 +6,18 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 import subprocess
 
-N = 228   # atm 6x6 ocn 6x6
+# N = 228   # atm 6x6 ocn 6x6
 # N = 318   # atm 6x6 ocn 9x9
-# N = 414   # atm 9x9 ocn 6x6
+N = 414   # atm 9x9 ocn 6x6
 DT = 1.0  # write interval in [timeunit]
-NT = 3544  # number of write. filesize = 8 * (N ** 2 + N) * NT [bytes]
+NT = 4000  # number of write. filesize = 8 * (N ** 2 + N) * NT [bytes]
 ONEDAY = 8.64  # [timeunit/day] a46p51
 
 def main():
     np.set_printoptions(formatter={'float': '{: 0.4f}'.format})
     mkdirs()
     fname = "evol_field_tlm.dat"
-    all_blv = np.empty((NT, N, N))
+    # all_blv = np.empty((NT, N, N))
     all_ble = np.zeros((NT, N))
     blv = np.random.normal(0.0, 1.0, (N, N))
     blv, ble = orth_norm_vectors(blv)
@@ -26,31 +26,31 @@ def main():
         blv = np.dot(tlm[:, :], blv)
         blv, ble = orth_norm_vectors(blv)
         all_ble[i, :] = ble[:] / (DT / ONEDAY)
-        all_blv[i, :, :] = blv[:, :]
+        # all_blv[i, :, :] = blv[:, :]
     mean_ble = np.mean(all_ble[NT//2:, :], axis=0)
     plt.plot(mean_ble)
     print(mean_ble)
     plt.savefig("img/tmp.pdf")
 
 def orth_norm_vectors(lv):
-  # lv     <- np.array[N,N] : Lyapunov vectors (column)
-  # return -> np.array[N,N] : orthonormalized LVs in descending order
-  # return -> np.array[N]      : ordered Lyapunov Exponents
+    # lv     <- np.array[N,N] : Lyapunov vectors (column)
+    # return -> np.array[N,N] : orthonormalized LVs in descending order
+    # return -> np.array[N]      : ordered Lyapunov Exponents
 
-  q, r = np.linalg.qr(lv)
+    q, r = np.linalg.qr(lv)
 
-  le = np.zeros(N)
-  eigvals = np.abs(np.diag(r))
+    le = np.zeros(N)
+    eigvals = np.abs(np.diag(r))
 
-  # for t-continuity, align
-  for i in range(N):
-    inner_prod = np.dot(q[:,i].T, lv[:,i])
-    if (inner_prod < 0):
-      q[:,i] *= -1.0
+    # for t-continuity, align
+    for i in range(N):
+        inner_prod = np.dot(q[:,i].T, lv[:,i])
+        if (inner_prod < 0):
+            q[:,i] *= -1.0
 
-  lv_orth = q
-  le = np.log(eigvals)
-  return lv_orth, le
+    lv_orth = q
+    le = np.log(eigvals)
+    return lv_orth, le
 
 def mkdirs():
     subprocess.run("rm -rf img", check=True, shell=True)
