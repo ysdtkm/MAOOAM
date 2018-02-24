35c1beae4942965b5b9d7ae498040ec22083a977
Takuma Yoshida <takuma@github.noreply>
2018-02-24 15:50:46 -0500

    mod

--- fortran/plot_tlm.py
+++ fortran/plot_tlm.py
@@ -5,7 +5,6 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 import subprocess
-import mydebug
 
 N = 36
 # N = 228   # atm 6x6 ocn 6x6
@@ -14,28 +13,61 @@ N = 36
 DT = 1.0  # write interval in [timeunit]
 NT = 1000  # number of write. filesize = 8 * (N ** 2 + N) * NT [bytes]
 ONEDAY = 8.64  # [timeunit/day] a46p51
+FNAME = "/lustre/tyoshida/shrt/exec/m200/evol_field_tlm.dat"
+LOGNORM = True
 
 def main():
     np.set_printoptions(formatter={'float': '{: 0.4f}'.format})
     mkdirs()
-    fname = "/lustre/tyoshida/shrt/exec/m200/evol_field_tlm.dat"
-    # all_blv = np.empty((NT, N, N))
-    all_ble = np.zeros((NT, N))
-    blv = np.random.normal(0.0, 1.0, (N, N))
-    blv, ble = orth_norm_vectors(blv)
-    tlms = np.empty((NT, N, N))
+    trajs, gs, ms, rs = integ_forward_ginelli()
+    cs = integ_backward_ginelli(rs)
+    vs = obtain_clvs_ginelli(cs, gs)
+    test_growth_rate(ms, vs)
+    test_growth_rate_long(ms, vs, 100, [0, 1, 24, 29])
+
+def integ_forward_ginelli():
+    # trajs[i, :] and gs[i, :, :] are at time i
+    # ms[i, :, :] and rs[i, :, :] are times i -> i + 1
+    g = np.random.normal(0.0, 1.0, (N, N))
+    g, r = np.linalg.qr(g)
+    trajs = np.empty((NT, N))
+    gs = np.empty((NT, N, N))
+    ms = np.empty((NT, N, N))
+    rs = np.empty((NT, N, N))
     for i in range(NT):
-        traj, tlm = read_file_part(fname, i)
-        tlms[i, :, :] = tlm
-        blv = np.dot(tlm[:, :], blv)
-        blv, ble = orth_norm_vectors(blv)
-        all_ble[i, :] = ble[:] / (DT / ONEDAY)
-        # all_blv[i, :, :] = blv[:, :]
-    mydebug.dump_array(tlms)
-    mean_ble = np.mean(all_ble[NT//2:, :], axis=0)
-    plt.plot(mean_ble)
-    print(mean_ble)
-    plt.savefig("img/tmp.pdf")
+        traj, m = read_file_part(FNAME, i)
+        trajs[i, :] = traj
+        gs[i, :, :] = g
+        g = np.dot(m, g)
+        g, r = np.linalg.qr(g)
+        ms[i, :, :] = m
+        rs[i, :, :] = r
+    return trajs, gs, ms, rs
+
+def integ_backward_ginelli(rs):
+    # rs[i, :, :] is times i -> i + 1
+    # cs[i, :, :] is at time i
+    cs = np.empty((NT, N, N))
+    c = np.random.normal(0.0, 1.0, (N, N))
+    dummy, c = np.linalg.qr(c)
+    c = normalize_column(c)
+    for i in reversed(range(NT)):
+        r = rs[i, :, :]
+        ri = np.linalg.inv(r)
+        c = np.dot(ri, c)
+        c = normalize_column(c)
+        cs[i, :, :] = c
+    return cs
+
+def obtain_clvs_ginelli(cs, gs):
+    # cs[i, :, :], gs[i, :, :], and vs[i, :, :] are at time i
+    vs = np.empty((NT, N, N))
+    for i in range(NT):
+        c = cs[i, :, :]
+        g = gs[i, :, :]
+        v = np.dot(g, c)
+        vs[i, :, :] = v
+    return vs
 
 def orth_norm_vectors(lv):
     # lv     <- np.array[N,N] : Lyapunov vectors (column)
@@ -47,6 +79,52 @@ def orth_norm_vectors(lv):
     lv_orth = q
     le = np.log(eigvals)
     return lv_orth, le
+
+def normalize_column(m):
+    for i in range(N):
+        c = m[:, i]
+        lc = np.dot(c, c) ** 0.5
+        m[:, i] /= lc
+    return m
+
+def test_growth_rate(ms, vs):
+    rate = [0.0 for j in range(N)]
+    for i in range(NT):
+        v = vs[i, :, :]
+        m = ms[i, :, :]
+        vn = np.dot(m, v)
+        for j in range(N):
+            r = np.log(np.linalg.norm(vn[:, j]) / np.linalg.norm(v[:, j]))
+            rate[j] += r / (DT / ONEDAY) / NT
+    print("Growth rate:")
+    print(rate)
+
+def test_growth_rate_long(ms, vs, ntg, ilist):
+    lengths = np.zeros((NT - ntg, ntg + 1, len(ilist)))
+    for k, i in enumerate(ilist):
+        for it in range(NT - ntg):
+            vi = vs[it, :, i]
+            lengths[it, 0, k] = np.linalg.norm(vi)
+            for jt in range(ntg):
+                vi = np.dot(ms[it + jt, :, :], vi[:])
+                lengths[it, jt + 1, k] = np.linalg.norm(vi)
+    growths = np.copy(lengths)
+    for jt in range(ntg):
+        growths[:, jt + 1, :] /= lengths[:, jt, :]
+    growths[:, 0, :] = np.nan
+    if LOGNORM:
+        growth_mean = np.mean(np.log(growths), axis=0) / (DT / ONEDAY)
+    else:
+        growth_mean = np.log(np.mean(growths, axis=0)) / (DT / ONEDAY)
+    plot_growth_rate(growth_mean, ilist)
+
+def plot_growth_rate(growth_mean, ilist):
+    # growth_mean.shape = (ntg + 1, len(ilist))
+    for k, i in enumerate(ilist):
+        plt.plot(growth_mean[:, k], label="CLV %02d" % (i + 1))
+    plt.legend()
+    plt.savefig("img/fig_8.png")
+    plt.close()
 
 def mkdirs():
     subprocess.run("rm -rf img", check=True, shell=True)
