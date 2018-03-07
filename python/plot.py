55633b0112db391a0728aa061824b0269f8c0a42
Takuma Yoshida <takuma@github.noreply>
2018-03-06 19:38:51 -0500

    de-vectorize

--- python/plot.py
+++ python/plot.py
@@ -9,7 +9,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 NMODEL = 36
 PINTVL = 10
-ANIMAX = 300
+ANIMAX = 3
 
 f0 = 1.032e-4
 g = 9.81
@@ -18,7 +18,7 @@ def main():
     mkdirs()
     nad, timd = read_file("evol_field.dat", 0.0)
     plot_time(nad, timd)
-    # plot_anime(nad)
+    plot_anime(nad)
     # plot_3d_trajectory(nad[:, 21], nad[:, 29], nad[:, 0])
 
 def mkdirs():
@@ -54,7 +54,7 @@ def plot_time(nad, timd):
 
 def plot_snap(cmaxs, nad, i):
     it = i // PINTVL
-    psia, ta, psio, to = reconstruct_grid(nad[i], 20, 20)
+    psia, ta, psio, to = all_reconstruct_grid(nad[i], 20, 20)
     datas = {"a_gph": psia * f0 / g, "a_t": ta, "o_psi": psio, "o_t": to}
     for cmp in cmaxs:
         title = "%s %04d" % (cmp, it)
@@ -99,71 +99,78 @@ def plot_matrix(mat, out, title="", cmax=None, ipol="none"):
     plt.savefig(out)
     plt.close()
 
-def reconstruct_grid(waves, nx, ny):
-    def fa(p):
-        # return np.ndarray[ny, nx]
-        return np.sqrt(2) * np.cos(p * y_grid)
-
-    def fk(m, p):
-        # return np.ndarray[ny, nx]
-        return 2.0 * np.cos(m * n * x_grid) * np.sin(p * y_grid)
-
-    def fl(h, p):
-        # return np.ndarray[ny, nx]
-        return 2.0 * np.sin(h * n * x_grid) * np.sin(p * y_grid)
-
-    def phio(ho, po):
-        # return np.ndarray[ny, nx]
-        return 2.0 * np.sin(0.5 * ho * n * x_grid) * np.sin(po * y_grid)
-
-    def atm(is_t):
-        # return np.ndarray[ny, nx]
+def reconstruct_grid(waves, x, y, elem):
+    # return np.arrays of atm_psi[K], atm_gph [m], ocn_psi[m2s-1], ocn_tmp [K]
+    def get_atm(is_temp):
+        types = ["A", "K", "L", "A", "K", "L", "K", "L", "K", "L"]
+        hs = [0, 1, 1, 0, 1, 1, 2, 2, 2, 2]
+        ps = [1, 1, 1, 2, 2, 2, 1, 1, 2, 2]
         gridval = 0.0
         for j in range(na):
-            j_all = j + na if is_t else j
+            j_all = j + na if is_temp else j
             if types[j] == "A":
-                gridval = gridval + waves[j_all] * fa(ps[j])
+                gridval = gridval + waves[j_all] * np.sqrt(2.0) * np.cos(ps[j] * y)
             elif types[j] == "K":
-                gridval = gridval + waves[j_all] * fk(hs[j], ps[j])
+                gridval = gridval + waves[j_all] * 2.0 * np.cos(hs[j] * n * x) * np.sin(ps[j] * y)
             else:
-                gridval = gridval + waves[j_all] * fl(hs[j], ps[j])
-        if is_t:
-            # gridval *= (2.0 * f0 / R)
+                gridval = gridval + waves[j_all] * 2.0 * np.sin(hs[j] * n * x) * np.sin(ps[j] * y)
+        if is_temp:
             gridval *= (f0 ** 2 * L ** 2) / R
         else:
             gridval *= L ** 2 * f0
         return gridval
 
-    def ocn(is_t):
-        # return np.ndarray[ny, nx]
+    def get_ocn(is_temp):
+        hos = [1, 1, 1, 1, 2, 2, 2, 2]
+        pos = [1, 2, 3, 4, 1, 2, 3, 4]
         gridval = 0.0
         for j in range(no):
-            j_all = j + (na * 2 + no) if is_t else j + na * 2
-            gridval = gridval + waves[j_all] * phio(hos[j], pos[j])
-        if is_t:
+            j_all = j + (na * 2 + no) if is_temp else j + na * 2
+            gridval = gridval + waves[j_all] * 2.0 * np.sin(0.5 * hos[j] * n * x) * np.sin(pos[j] * y)
+        if is_temp:
             gridval *= (f0 ** 2 * L ** 2) / R
         else:
             gridval -= np.mean(gridval)
             gridval *= L ** 2 * f0
         return gridval
 
-    n = 1.5
-    x_grid = np.empty((ny, nx))
-    x_grid[:, :] = np.linspace(0, 2.0 * np.pi / n, nx)[np.newaxis, :]
-    y_grid = np.empty((ny, nx))
-    y_grid[:, :] = np.linspace(np.pi, 0, ny)[:, np.newaxis]
+    assert waves.__class__ == np.ndarray
+    assert y.__class__ in [float, np.float32, np.float64]
+    assert x.__class__ in [float, np.float32, np.float64]
+    assert waves.shape == (36,)
 
+    n = 1.5
     na = 10
     no = 8
-    R = 287
-    L = 5000000 / np.pi
-    types = ["A", "K", "L", "A", "K", "L", "K", "L", "K", "L"]
-    hs = [0, 1, 1, 0, 1, 1, 2, 2, 2, 2]
-    ps = [1, 1, 1, 2, 2, 2, 1, 1, 2, 2]
-    hos = [1, 1, 1, 1, 2, 2, 2, 2]
-    pos = [1, 2, 3, 4, 1, 2, 3, 4]
+    R = 287.0
+    L = 5000000.0 / np.pi
 
-    return atm(False), atm(True), ocn(False), ocn(True)
+    if elem == "a_gph":
+        return get_atm(False)
+    elif elem == "a_tmp":
+        return get_atm(True)
+    elif elem == "o_psi":
+        return get_ocn(False)
+    elif elem == "o_tmp":
+        return get_ocn(True)
+    else:
+        raise Exception("reconstruct_grid overflow. Element %s not found." % elem)
+
+def all_reconstruct_grid(waves, nx, ny):
+    n = 1.5
+    x_grid = np.linspace(0, 2.0 * np.pi / n, nx)
+    y_grid = np.linspace(np.pi, 0, ny)
+    a_gph = np.empty((ny, nx))
+    a_tmp = np.empty((ny, nx))
+    o_psi = np.empty((ny, nx))
+    o_tmp = np.empty((ny, nx))
+    for ix in range(nx):
+        for iy in range(ny):
+            a_gph[iy, ix] = reconstruct_grid(waves, x_grid[ix], y_grid[iy], "a_gph")
+            a_tmp[iy, ix] = reconstruct_grid(waves, x_grid[ix], y_grid[iy], "a_tmp")
+            o_psi[iy, ix] = reconstruct_grid(waves, x_grid[ix], y_grid[iy], "o_psi")
+            o_tmp[iy, ix] = reconstruct_grid(waves, x_grid[ix], y_grid[iy], "o_tmp")
+    return a_gph, a_tmp, o_psi, o_tmp
 
 if __name__ == "__main__":
     main()
